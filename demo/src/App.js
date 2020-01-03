7aa506ad8807c1b67353495a23d3022da092386c
Gustavo Esquinca <g.esq.ca@gmail.com>
2020-01-03 14:31:06 -0500

    Revert "Edit App template design"
    This reverts commit 8a7fafee31af836236e016ed1f3655b3c6dc333d.

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
 import {
+  ThemeProvider,
   theme,
   BaseStyles,
   Flex,
-  Text,
-  Heading,
-  Input
+  Box,
+  Field,
+  Input,
+  Heading
 } from "rimble-ui";
 import * as icons from "@rimble/icons";
+import styled from "styled-components";
 
-const Truncate = styled(Text)([], {
+const Truncate = styled(Box)([], {
   overflow: "hidden",
   whiteSpace: "nowrap",
   textOverflow: "ellipsis"
@@ -21,60 +23,52 @@ const IconList = ({ filterValue }) => {
   return Object.keys(icons)
     .filter(key => key !== "Icon" && key.includes(filterValue))
     .map(key => (
-      <Text
+      <Box
+        width={"4rem"}
         key={key}
         title={key}
-        width={'5rem'}
         m={3}
-        textAlign={'center'}
+        style={{
+          textAlign: "center"
+        }}
       >
         {React.createElement(icons[key], {
           title: key,
           size: 48
         })}
-        <Truncate lineHeight={'1rem'} fontSize={1}>
-          {key}
-        </Truncate>
-      </Text>
+        <Truncate fontSize={"1rem"}>{key}</Truncate>
+      </Box>
     ));
 };
 
 const FilteredIcons = () => {
   const [filterValue, setFilterValue] = useState("");
   return (
-    <>
-      <Input
-        type="search"
-        placeholder="Filter…"
-        onChange={e => setFilterValue(e.target.value)}
-        value={filterValue}
-        width={'100%'}
-        maxWidth={'50%'}
-        mx={'auto'}
-        mb={5}
-      />
-      <Flex flexWrap={'wrap'} justifyContent={'center'} >
+    <Box my={4}>
+      <Field label="Filter icons">
+        <Input
+          type="text"
+          placeholder="e.g. Eth"
+          onChange={e => setFilterValue(e.target.value)}
+          value={filterValue}
+        />
+      </Field>
+
+      <Flex flexWrap="wrap">
         <IconList filterValue={filterValue} />
       </Flex>
-    </>
+    </Box>
   );
 };
 
 const App = props => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} className="App">
       <BaseStyles>
-        <Flex
-          flexDirection={'column'}
-          alignItems={'center'}
-          maxWidth={'80vw'}
-          mx={'auto'}
-        >
-          <Heading as={'h1'} py={5}>
-            Rimble Icons
-          </Heading>
+        <Box m={4}>
+          <Heading as="h1">Rimble Icons</Heading>
           <FilteredIcons icons={icons} />
-        </Flex>
+        </Box>
       </BaseStyles>
     </ThemeProvider>
   );
